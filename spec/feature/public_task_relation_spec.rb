aac47c65cba54ea530a922383277bf7cc9fff503
Cássio Oseki <emp.justine@gmail.com>
2018-02-27 22:41:20 +0000

    Testes funcionais para features 4 e 6.

--- spec/feature/public_task_relation_spec.rb
+++ spec/feature/public_task_relation_spec.rb
@@ -1,29 +1,25 @@
-feature 'task relations can be public or private' do
-  context 'new user experience' do
-    scenario "without task relations" do
-      sign_up_with('teste@teste.com', '123456')
-      expect(page).to have_content('Sign out')
-
-      visit '/task_relations'
-      expect(page).to have_content('Você não criou nenhuma lista de tarefa.')
-      expect(page).to have_content('Não existem listas de tarefas marcadas como favoritas.')
-    end
-
-    scenario "with private task relation" do
-      sign_up_with('teste@teste.com', '123456')
+feature 'task relations' do
+  context 'can be public or private' do
+    scenario "hides private task relation" do
+      sign_up_with('alice_private@teste.com', '123456')
       create_task_relation()
-
       visit '/task_relations'
       expect(page).to_not have_content('Você não criou nenhuma lista de tarefa.')
+      sign_out()
 
+      sign_up_with('mallory@teste.com', '123456')
+      expect(page).to_not have_content('alice_private@teste.com')
     end
 
-    scenario "with private task relation" do
-      sign_up_with('teste@teste.com', '123456')
-      create_task_relation()
-
+    scenario "shows public task relation" do
+      sign_up_with('alice_public@teste.com', '123456')
+      create_task_relation(public: true)
       visit '/task_relations'
       expect(page).to_not have_content('Você não criou nenhuma lista de tarefa.')
+      sign_out()
+
+      sign_up_with('bob@teste.com', '123456')
+      expect(page).to have_content('alice_public@teste.com')
     end
   end
 end
